4c5f160be3aca7cad1f681c9ef66bd15c71d5f85
Adphi <philippe.adrien.nousse@gmail.com>
2024-11-04 17:01:34 +0100

    remove embedded registry support
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- pkg/agent/run.go
+++ pkg/agent/run.go
@@ -34,7 +34,6 @@ import (
 	"github.com/k3s-io/k3s/pkg/profile"
 	"github.com/k3s-io/k3s/pkg/rootless"
 	"github.com/k3s-io/k3s/pkg/signals"
-	"github.com/k3s-io/k3s/pkg/spegel"
 	"github.com/k3s-io/k3s/pkg/util"
 	"github.com/k3s-io/k3s/pkg/version"
 	pkgerrors "github.com/pkg/errors"
@@ -109,16 +108,6 @@ func run(ctx context.Context, cfg cmds.Agent, proxy proxy.Proxy) error {
 
 	if err := executor.Bootstrap(ctx, nodeConfig, cfg); err != nil {
 		return err
-	}
-
-	if nodeConfig.EmbeddedRegistry {
-		if nodeConfig.ContainerRuntimeEndpoint != "" {
-			return errors.New("embedded registry mirror requires embedded containerd")
-		}
-
-		if err := spegel.DefaultRegistry.Start(ctx, nodeConfig, executor.CRIReadyChan()); err != nil {
-			return pkgerrors.WithMessage(err, "failed to start embedded registry")
-		}
 	}
 
 	if nodeConfig.SupervisorMetrics {
